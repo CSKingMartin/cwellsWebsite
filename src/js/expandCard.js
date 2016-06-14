6b396fbe4c56cc28660ee575318fb3a11e0e3e21
CSKingMartin <cwells@linfield.edu>
2016-06-14 15:43:32 -0700

    continued work on content/profile card

---
+++ src/js/expandCard.js
@@ -0,0 +1,22 @@
+		var tf = "0";
+
+		$(".expButton").click(function(){
+
+			console.log("onClick");
+
+			$(".collapsedCard").toggleClass("expandedCard");
+
+
+			if (tf == "0") {
+
+				$(".expButton").html("<h2>&#10006</h2>");
+
+				tf = "1";
+			} 
+			else {
+
+				$(".expButton").html("<h1>&#8942</h1>");
+
+				tf = "0";
+			}
+	});
--- src/js/getArticle.js
+++ src/js/getArticle.js
@@ -1,12 +1,14 @@
-$(".button").click(function(){
-	var id = $(this).attr('aValue');
-    $.ajax({ url: './articles/_article' + id + '.html',
-        success: function(str) {
-        	//alert(str);
-            $("#mainContent").html(str);
-        },
-        error: function(err) {
-            alert("I'm sorry, that article doesn't exist!");
-        }
-	});
+$(function() {
+    $(".button").click(function(){
+        var id = $(this).attr('aValue');
+        $.ajax({ url: './articles/_article' + id + '.html',
+            success: function(str) {
+                //alert(str);
+                $("#mainContent").html(str);
+            },
+            error: function(err) {
+                alert("I'm sorry, that article doesn't exist!");
+            }
+        });
+    });
 });

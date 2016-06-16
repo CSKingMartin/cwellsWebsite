c228a8bb8f7a20c9993c188a604ba95ca26d18cf
CSKingMartin <cwells@linfield.edu>
2016-06-16 09:16:38 -0700

    hopefully new thing?

--- js/expandCard.js
+++
@@ -1,1 +0,0 @@
-var tf="0";$(".expButton").click(function(){console.log("onClick"),$(".collapsedCard").toggleClass("expandedCard"),"0"==tf?($(".expButton").html("<h2>&#10006</h2>"),tf="1"):($(".expButton").html("<h1>&#8942</h1>"),tf="0")});

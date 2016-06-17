3fa598b1a9b77c5bd1ac91b00e3935ebeee08e2f
CSKingMartin <cwells@linfield.edu>
2016-06-17 13:32:02 -0700

    newest build with sidebar changes

--- js/expandCard.js
+++ js/expandCard.js
@@ -1,1 +1,1 @@
-var tf="0";$(".expButton").click(function(){console.log("onClick"),$(".collapsedCard").toggleClass("expandedCard"),"0"==tf?($(".expButton").html("<h2>&#10006</h2>"),tf="1"):($(".expButton").html("<h1>&#8942</h1>"),tf="0")});
+var tf="0";$(".expButton").click(function(){console.log("onClick"),$(".expMenu").toggleClass("expMenu-exp"),"0"==tf?($(".expButton").html("<h2>&#10006</h2>"),tf="1"):($(".expButton").html("<h1>&#8942</h1>"),tf="0")});

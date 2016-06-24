1babb12ab7b9cda340fb9439de894aed1b5c50d9
CSKingMartin <cwells@linfield.edu>
2016-06-24 16:22:47 -0700

    comments no-go

--- src/js/getArticle.js
+++ src/js/getArticle.js
@@ -1,24 +1,29 @@
-
-
 function loadArticle(index) {
-    $.ajax({ url: './articles/article' + index + '.html',
-            success: function(str) {
-                $("#mainContent").html(str);
-            },
-            error: function(err) {
-                alert("I'm sorry, that article doesn't exist!");
-            }
-        });
+	$.ajax({ url: './articles/article' + index + '.html',
+			success: function(str) {
+				$("#mainContent").html(str);
+				DISQUS.reset({
+					reload: true,
+					config: function () {  
+					this.page.identifier = "article" + index;  
+					this.page.url = "http://cskingmartin.github.io/cwellsWebsite/news.html#!article" + index;
+				  }
+				});
+			},
+			error: function(err) {
+				alert("I'm sorry, that article doesn't exist!");
+			}
+		});
 
 }
 
 $(function() {
-    $(window).on('hashchange', function() {
-        var index = location.hash;
-        var i = index.slice(-1);
-        //alert(i);
+	$(window).on('hashchange', function() {
+		var index = location.hash;
+		var i = index.slice(-1);
+		//alert(i);
 
-        loadArticle(i);
-    });
+		loadArticle(i);
+	});
 });
 
